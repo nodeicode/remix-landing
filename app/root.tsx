b1c190b73716870b46f0c44be013042c52527830
nodeicode <lohitaryan20@gmail.com>
2022-06-12 13:29:44 -0400

    feat: full site with dark more

--- app/root.tsx
+++ app/root.tsx
@@ -1,32 +1,30 @@
 import type { MetaFunction } from "@remix-run/node";
-import {
-  Links,
-  LiveReload,
-  Meta,
-  Outlet,
-  Scripts,
-  ScrollRestoration,
-} from "@remix-run/react";
+import { Links, LiveReload, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
+import type { LinksFunction } from "@remix-run/node";
+import styles from "./tailwind.css";
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: styles }];
 
 export const meta: MetaFunction = () => ({
-  charset: "utf-8",
-  title: "New Remix App",
-  viewport: "width=device-width,initial-scale=1",
+	charset: "utf-8",
+	title: "Lohit Aryan",
+	viewport: "width=device-width,initial-scale=1",
+	description: "Well this is a site about me, what else did you expect?",
 });
 
 export default function App() {
-  return (
-    <html lang="en">
-      <head>
-        <Meta />
-        <Links />
-      </head>
-      <body>
-        <Outlet />
-        <ScrollRestoration />
-        <Scripts />
-        <LiveReload />
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<head>
+				<Meta />
+				<Links />
+			</head>
+			<body>
+				<Outlet />
+				<ScrollRestoration />
+				<Scripts />
+				<LiveReload />
+			</body>
+		</html>
+	);
 }
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,32 +1,87 @@
+import type { LinksFunction, MetaFunction } from "@remix-run/react/routeModules";
+import { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSun, faMoon } from "@fortawesome/free-solid-svg-icons";
+import TypewriterComponent from "typewriter-effect";
+
+export const links: LinksFunction = () => {
+	return [
+		{
+			rel: "preload",
+			href: "/coding_light.svg",
+			as: "image",
+		},
+		{
+			rel: "preload",
+			href: "/coding_dark.svg",
+			as: "image",
+		},
+	];
+};
+
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+	const [darkMode, setTheme] = useState(false);
+	const getImage = (): string => {
+		return darkMode ? "/coding_light.svg" : "/coding_dark.svg";
+	};
+	const getTextColor = (): string => {
+		return darkMode ? "text-light" : "text-dark";
+	};
+	const toggleTheme = () => {
+		document.querySelector("body")?.classList.toggle("dark");
+		setTheme(!darkMode);
+	};
+	const getIcon = () => {
+		return darkMode ? faSun : faMoon;
+	};
+
+	return (
+		<div className="flex h-screen flex-col items-center justify-center gap-8 bg-light transition-all dark:bg-dark sm:flex-row">
+			<img src={getImage()} alt="coding" className="w-[40vh] lg:w-[40vw]" />
+			<div className="prose prose-stone px-4 font-mono prose-a:text-blue-600 dark:prose-invert sm:px-2">
+				<FontAwesomeIcon
+					className={`w-8 transition-colors hover:cursor-pointer hover:text-gray ${getTextColor()}`}
+					icon={getIcon()}
+					onClick={() => toggleTheme()}
+				/>
+				<h2>Hello there! let me introduce myself</h2>
+				<h1>
+					I'm{" "}
+					<TypewriterComponent
+						onInit={(typewriter) => {
+							typewriter
+								.typeString("a Web Dev")
+								.pauseFor(1000)
+								.deleteAll()
+								.typeString("a Gamer")
+								.pauseFor(1000)
+								.deleteAll()
+								.typeString("a Kpop fan")
+								.pauseFor(500)
+								.deleteAll()
+								.typeString("Lohit Aryan")
+								.pause()
+								.start();
+						}}
+					/>
+				</h1>
+				<p className="lead">Turning some idea into a web app or just watching netflix</p>
+				<p>
+					Here is my{" "}
+					<a rel="noopener" target="_blank" href="https://github.com/nodeicode">
+						Github
+					</a>{" "}
+					its been quiet for a while there <br />
+					<a rel="noopener" target="_blank" href="https://twitter.com/nodeicode">
+						Twitter
+					</a>{" "}
+					casual convo?,{" "}
+					<a rel="noopener" target="_blank" href="https://www.linkedin.com/in/lohit-aryan/">
+						Linkedin
+					</a>{" "}
+					if that's your thing
+				</p>
+			</div>
+		</div>
+	);
 }

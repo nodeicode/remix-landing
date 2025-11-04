a3d368dbca4f2e85c5e315ab8f81edfee7c9c08d
nodeicode <lohitaryan20@gmail.com>
2025-11-03 20:20:18 -0500

    chore: remove test-notif api endpoint

--- app/routes.ts
+++ app/routes.ts
@@ -6,6 +6,5 @@ export default [
   route("api/positions", "routes/api.positions.ts"),
   route("api/trigger-push", "routes/api.trigger-push.ts"),
   route("api/subscribe", "routes/api.subscribe.ts"),
-  route("api/test-notification", "routes/api.test-notification.ts"),
   route("api/cleanup-subscriptions", "routes/api.cleanup-subscriptions.ts"),
 ] satisfies RouteConfig;
